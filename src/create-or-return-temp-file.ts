9aaef48776290a7011cbb92193f1d7bd19727ba7
Ti/o <tilo.codes@gmail.com>
2021-12-27 20:39:36 +0100

    Fix to make it runnable on linux (WSL)

--- src/create-or-return-temp-file.ts
+++ src/create-or-return-temp-file.ts
@@ -1,56 +1,68 @@
 import { join } from "https://deno.land/std/path/mod.ts";
 import { Resource } from "./resource.ts";
-import { ensureDir, exists } from "https://deno.land/std@0.78.0/fs/mod.ts";
+import {
+  ensureDir,
+  exists,
+  emptyDir,
+} from "https://deno.land/std@0.78.0/fs/mod.ts";
 import { sleep } from "./sleep.ts";
+import tempDirectory from "https://deno.land/x/temp_dir@v1.0.0/mod.ts";
 
-const tempDir = Deno.env.get('TMPDIR')!;
-
-const tempDirPath = join(tempDir, 't_env_paral');
+const tempDirPath = join(tempDirectory, "t_env_paral");
 const errorCodeAlreadyExists = "EEXIST";
 
 export type CreateOrReturnTempFileOptions = {
-    waitTimeout?: number;
-    maxWaitLoops?: number
-}
+  waitTimeout?: number;
+  maxWaitLoops?: number;
+};
 
-export const cleanTemp = () => Deno.remove(tempDirPath, { recursive: true });
-export const createOrReturnTempFile = async (resource: Resource, options?: CreateOrReturnTempFileOptions): Promise<any> => {
+export const cleanTemp = () => emptyDir(tempDirPath);
+export const createOrReturnTempFile = async (
+  resource: Resource,
+  options?: CreateOrReturnTempFileOptions
+): Promise<any> => {
+  await ensureDir(tempDirPath);
+  const tempPath = join(tempDirPath, resource.name);
+  const tempLockPath = join(tempDirPath, resource.name + ".lock");
 
-    await ensureDir(tempDirPath);
-    const tempPath = join(tempDirPath, resource.name);
-    const tempLockPath = join(tempDirPath, resource.name + '.lock');
+  let lockFileCreated = false;
 
-    let lockFileCreated = false;
-
+  try {
     try {
-        try {
-            const lockFile = await Deno.open(tempLockPath, { createNew: true, write: true });
-            lockFile.close();
-            lockFileCreated = true;
-        } catch (error) {
+      const lockFile = await Deno.open(tempLockPath, {
+        createNew: true,
+        write: true,
+      });
+      lockFile.close();
+      lockFileCreated = true;
+    } catch (error) {
+      if (error.code !== errorCodeAlreadyExists) throw error;
 
-            if (error.code !== errorCodeAlreadyExists) throw error;
+      let counter = 0;
+      const maxCounter = options?.maxWaitLoops ?? 1000;
+      const waitTime = options?.waitTimeout ?? 100;
 
-            let counter = 0;
-            const maxCounter = options?.maxWaitLoops ?? 1000;
-            const waitTime = options?.waitTimeout ?? 100;
+      let doesLockExist = await exists(tempLockPath);
+      while (doesLockExist) {
+        if (counter > maxCounter)
+          throw new Error(
+            `Lock file ${tempLockPath} was not removed in time. Waited ${
+              (counter * waitTime) / 1000
+            } seconds.`
+          );
+        counter++;
+        await sleep(waitTime);
+        doesLockExist = await exists(tempLockPath);
+      }
 
-            let doesLockExist = await exists(tempLockPath);
-            while (doesLockExist) {
-                if (counter > maxCounter) throw new Error(`Lock file ${tempLockPath} was not removed in time. Waited ${counter * waitTime / 1000} seconds.`)
-                counter++;
-                await sleep(waitTime);
-                doesLockExist = await exists(tempLockPath);
-            }
-
-            const fileBuffer = await Deno.readTextFile(tempPath);
-            return { data: JSON.parse(fileBuffer), path: tempPath, created: false };
-        }
-
-        const data = await resource.creatorFn();
-        await Deno.writeTextFile(tempPath, JSON.stringify(data));
-        return { path: tempPath, data, created: true };
-    } finally {
-        if (lockFileCreated) await Deno.remove(tempLockPath);
+      const fileBuffer = await Deno.readTextFile(tempPath);
+      return { data: JSON.parse(fileBuffer), path: tempPath, created: false };
     }
-}
+
+    const data = await resource.creatorFn();
+    await Deno.writeTextFile(tempPath, JSON.stringify(data));
+    return { path: tempPath, data, created: true };
+  } finally {
+    if (lockFileCreated) await Deno.remove(tempLockPath);
+  }
+};
